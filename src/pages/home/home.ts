9be9f7730def9b61f822e6408329b9cd8cd6106d
Matthieu Drula <matthieu.drula.hs@gmail.com>
2017-10-08 17:04:37 -0400

    all working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, NgZone } from '@angular/core';
+
+import { Platform } from 'ionic-angular';
+
+declare var ApiAIPromises: any;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  answers = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public platform: Platform, public ngZone: NgZone) {
+    platform.ready().then(() => {
 
+      ApiAIPromises.init({
+        clientAccessToken: "996215250fd4438db3ff5cf9d5957c3e"
+      })
+      .then((result) =>  console.log(result))
+
+    });
   }
 
+  ask(question) {
+    ApiAIPromises.requestText({
+      query: question
+    })
+    .then(({result: {fulfillment: {speech}}}) => {
+       this.ngZone.run(()=> {
+         this.answers.push(speech);
+       });
+    })
+  }
 }
